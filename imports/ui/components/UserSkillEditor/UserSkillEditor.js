77dddd4694e29247dd17fe6dd8c1c10ba284fd37
Terry Tompkins <tompkins@transwebtools.com>
2017-12-28 20:08:18 -0500

    updated with a test grid built from react-bootstrap-table

--- imports/ui/components/UserSkillEditor/UserSkillEditor.js
+++ imports/ui/components/UserSkillEditor/UserSkillEditor.js
@@ -1,11 +1,64 @@
 /* eslint-disable max-len, no-return-assign */
 
+import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
+import '../../../../node_modules/react-bootstrap-table/dist/react-bootstrap-table-all.min.css';
+
 import React from 'react';
 import PropTypes from 'prop-types';
 import { FormGroup, ControlLabel, Button } from 'react-bootstrap';
 import { Meteor } from 'meteor/meteor';
 import { Bert } from 'meteor/themeteorchef:bert';
 import validate from '../../../modules/validate';
+
+const jobs = [];
+const jobTypes = [ 'A', 'B', 'C', 'D' ];
+
+function addJobs(quantity) {
+  const startId = jobs.length;
+  for (let i = 0; i < quantity; i++) {
+    const id = startId + i;
+    jobs.push({
+      id: id,
+      status: '200',
+      name: 'Item name ' + id,
+      type: 'B',
+      active: i % 2 === 0 ? 'Y' : 'N'
+    });
+  }
+}
+
+addJobs(5);
+
+const cellEditProp = {
+  mode: 'click',
+  blurToSave: true
+};
+
+// validator function pass the user input value and should return true|false.
+function jobNameValidator(value) {
+  const response = { isValid: true, notification: { type: 'success', msg: '', title: '' } };
+  if (!value) {
+    response.isValid = false;
+    response.notification.type = 'error';
+    response.notification.msg = 'Value must be inserted';
+    response.notification.title = 'Requested Value';
+  } else if (value.length < 10) {
+    response.isValid = false;
+    response.notification.type = 'error';
+    response.notification.msg = 'Value must have 10+ characters';
+    response.notification.title = 'Invalid Value';
+  }
+  return response;
+}
+
+function jobStatusValidator(value) {
+  const nan = isNaN(parseInt(value, 10));
+  if (nan) {
+    return 'Job Status must be a integer!';
+  }
+  return true;
+}
+
 
 class UserSkillEditor extends React.Component {
   componentDidMount() {
@@ -54,35 +107,28 @@ class UserSkillEditor extends React.Component {
     });
   }
 
+  invalidJobStatus(cell, row) {
+    console.log(`${cell} at row id: ${row.id} fails on editing`);
+    return 'invalid-jobstatus-class';
+  }
+
+  editingJobStatus(cell, row) {
+    console.log(`${cell} at row id: ${row.id} in current editing`);
+    return 'editing-jobstatus-class';
+  }
+
+
+
   render() {
     const { doc } = this.props;
     return (
-      <form ref={form => (this.form = form)} onSubmit={event => event.preventDefault()}>
-        <FormGroup>
-          <ControlLabel>Title</ControlLabel>
-          <input
-            type="text"
-            className="form-control"
-            name="title"
-            ref={title => (this.title = title)}
-            defaultValue={doc && doc.title}
-            placeholder="Oh, The Places You'll Go!"
-          />
-        </FormGroup>
-        <FormGroup>
-          <ControlLabel>Body</ControlLabel>
-          <textarea
-            className="form-control"
-            name="body"
-            ref={body => (this.body = body)}
-            defaultValue={doc && doc.body}
-            placeholder="Congratulations! Today is your day. You're off to Great Places! You're off and away!"
-          />
-        </FormGroup>
-        <Button type="submit" bsStyle="success">
-          {doc && doc._id ? 'Save Changes' : 'Add UserSkill'}
-        </Button>
-      </form>
+        <BootstrapTable data={ jobs } cellEdit={ cellEditProp } insertRow={ true }>
+             <TableHeaderColumn dataField='id' isKey={ true }>Job ID</TableHeaderColumn>
+             <TableHeaderColumn dataField='status' editable={ { validator: jobStatusValidator } } editColumnClassName={ this.editingJobStatus } invalidEditColumnClassName={ this.invalidJobStatus }>Job Status</TableHeaderColumn>
+             <TableHeaderColumn dataField='name' editable={ { type: 'textarea', validator: jobNameValidator } } editColumnClassName='editing-jobsname-class' invalidEditColumnClassName='invalid-jobsname-class'>Job Name</TableHeaderColumn>
+             <TableHeaderColumn dataField='type' editable={ { type: 'select', options: { values: jobTypes } } }>Job Type</TableHeaderColumn>
+             <TableHeaderColumn dataField='active' editable={ { type: 'checkbox', options: { values: 'Y:N' } } }>Active</TableHeaderColumn>
+   </BootstrapTable>
     );
   }
 }
@@ -97,3 +143,34 @@ UserSkillEditor.propTypes = {
 };
 
 export default UserSkillEditor;
+
+
+{/*
+
+    <form ref={form => (this.form = form)} onSubmit={event => event.preventDefault()}>
+      <FormGroup>
+        <ControlLabel>Title</ControlLabel>
+        <input
+          type="text"
+          className="form-control"
+          name="title"
+          ref={title => (this.title = title)}
+          defaultValue={doc && doc.title}
+          placeholder="Oh, The Places You'll Go!"
+        />
+      </FormGroup>
+      <FormGroup>
+        <ControlLabel>Body</ControlLabel>
+        <textarea
+          className="form-control"
+          name="body"
+          ref={body => (this.body = body)}
+          defaultValue={doc && doc.body}
+          placeholder="Congratulations! Today is your day. You're off to Great Places! You're off and away!"
+        />
+      </FormGroup>
+      <Button type="submit" bsStyle="success">
+        {doc && doc._id ? 'Save Changes' : 'Add UserSkill'}
+      </Button>
+    </form>
+*/}

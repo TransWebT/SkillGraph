d257c1945bad34e979cb37b9218f33110f4c5389
Terry Tompkins <tompkins@transwebtools.com>
2018-03-11 18:23:41 -0400

    added test graph code for Rechart package

--- imports/ui/pages/UserSkills/UserSkills.js
+++ imports/ui/pages/UserSkills/UserSkills.js
@@ -8,8 +8,19 @@ import { Bert } from 'meteor/themeteorchef:bert';
 import UserSkillsCollection from '../../../api/UserSkills/UserSkills';
 import { timeago, monthDayYearAtTime } from '../../../modules/dates';
 import Loading from '../../components/Loading/Loading';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 
 import './UserSkills.scss';
+
+const data = [
+      {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
+      {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
+      {name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
+      {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
+      {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
+      {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
+      {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
+];
 
 const handleRemove = (userSkillId) => {
   if (confirm('Are you sure? This is permanent!')) {
@@ -31,6 +42,20 @@ const UserSkills = ({
       <h4 className="pull-left">UserSkills</h4>
       <Link className="btn btn-success pull-right" to={`${match.url}/new`}>Add UserSkill</Link>
     </div>
+
+    <div className="UserSkills">
+      <LineChart width={600} height={300} data={data}
+            margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+       <XAxis dataKey="name"/>
+       <YAxis/>
+       <CartesianGrid strokeDasharray="3 3"/>
+       <Tooltip/>
+       <Legend />
+       <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{r: 8}}/>
+       <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+      </LineChart>
+    </div>
+
     {userSkills.length ?
       <Table responsive>
         <thead>

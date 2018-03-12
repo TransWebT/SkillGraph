8e6a3f880091d7930ebd34fcda7cb22b2e49c0fe
Terry Tompkins <tompkins@transwebtools.com>
2018-03-12 18:59:32 -0400

    more rechart testing

--- imports/ui/pages/UserSkills/UserSkills.js
+++ imports/ui/pages/UserSkills/UserSkills.js
@@ -59,8 +59,8 @@ const UserSkills = ({
       <LineChart width={600} height={300} data={data}
             margin={{top: 5, right: 30, left: 20, bottom: 5}}>
        <XAxis dataKey="date"/>
-       <YAxis/>
-       <CartesianGrid strokeDasharray="3 3"/>
+       <YAxis type="number" interval="1" domain={[0, 10]}/>
+       <CartesianGrid strokeDasharray="1 1"/>
        <Tooltip/>
        <Legend />
        <Line type="monotone" dataKey="java" stroke="#8884d8" activeDot={{r: 8}}/>
